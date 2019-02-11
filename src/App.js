9f6d87eaf3da958e0357af846e46ac8ee6dfe9fc
Jethro Ordaneza <jet.oneza@gmail.com>
2019-02-11 14:37:03 +0800

    Use AppLayout in main App component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
 
-import Container from './common/Container'
+// Layout Components
+import AppLayout from './layouts/AppLayout';
 
 export default function App() {
   return (
-    <Container
-      fluid
-      style={{
-        padding: 24,
-        textAlign: 'center'
-      }}
-    >
-      <h1>
-        RE Customers
-      </h1>
-      <button className="btn btn-lg btn-success">Success Button</button>
-    </Container>
+    <AppLayout />
   );
 }
