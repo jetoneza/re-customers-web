5eadf11c95da548b1c52436a4112150d449bf18e
Jethro Ordaneza <jet.oneza@gmail.com>
2019-02-11 14:36:53 +0800

    Create AppLayout component

---
+++ src/layouts/AppLayout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Implemented Components
+import NavBar from '../components/NavBar';
+
+// Route Components
+import Dashboard from '../routes/Dashboard';
+
+export default function AppLayout() {
+  return (
+    <React.Fragment>
+      <NavBar />
+      <Dashboard />
+    </React.Fragment>
+  );
+}
