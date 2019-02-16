969dc81403208bdb18717b3b45f06547ee262877
Jethro Ordaneza <jet.oneza@gmail.com>
2019-02-16 14:57:11 +0800

    Create constants for colors

---
+++ src/constants/colors.js
@@ -0,0 +1,37 @@
+export const colors = {
+  // Primary shades
+  blueBerry: '#4392F1',
+  blueBerryLight: '#549BF2',
+  blueBerryLighter: '#65A5F3',
+  blueBerryLightest: '#76AFF4',
+  blueBerryDark: '#3D85DC',
+  blueBerryDarker: '#3778C6',
+  blueBerryDarkest: '#316BB0',
+
+  // White-Black shades
+  white: '#FFFFFF',
+  platinum: '#E8E8E8',
+  gray: '#BABABA',
+  grayLight: '#D1D1D1',
+  grayDark: '#A3A3A3',
+  grayDarkest: '#393939',
+  black: '#262626',
+
+  // Secondary shades
+  seaBlue: '#2E80A7',
+  seaBlueLight: '#458EB1',
+  seaBlueLighter: '#5C9CBA',
+  seaBlueLightest: '#73AAC4',
+  seaBlueDark: '#17729D',
+  seaBlueDarker: '#006494',
+  seaBlueDarkest: '#005B87',
+
+  // Accent shades
+  vermillion: '#DC493A',
+  vermillionLight: '#DF594B',
+  vermillionLighter: '#E26A5D',
+  vermillionLightest: '#E57A6F',
+  vermillionDark: '#C84335',
+  vermillionDarker: '#B53C30',
+  vermillionDarkest: '#A1362B',
+};
