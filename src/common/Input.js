acb2a8cbc215c422a913d98ef42e26c6a5b17041
Jethro Ordaneza <jet.oneza@gmail.com>
2019-02-17 00:11:25 +0800

    Create Input component

---
+++ src/common/Input.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Input({
+  label,
+  name,
+  onChange,
+  placeholder,
+  type,
+}) {
+  return (
+    <div className="form-group">
+      { label && <label htmlFor={name}>{label}</label> }
+      <input
+        type={type}
+        className="form-control"
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+Input.propTypes = {
+  label: PropTypes.string,
+  name: PropTypes.string,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
+  type: PropTypes.string,
+};
+
+Input.defaultProps = {
+  label: null,
+  name: '',
+  onChange: () => {},
+  placeholder: 'Enter text',
+  type: 'text',
+};
+
+export default Input;
