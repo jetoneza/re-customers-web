d28287b07ef007806f775c4ffe733c2879a38847
Jethro Ordaneza <jet.oneza@gmail.com>
2019-02-16 23:05:00 +0800

    Create GlobalStyle

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import {BrowserRouter} from 'react-router-dom'
 // Layout Components
 import AppLayout from 'layouts/AppLayout';
 
+// Styled Components
+import GlobalStyle from 'common/GlobalStyle';
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <AppLayout />
-    </BrowserRouter>
+    <React.Fragment>
+      <GlobalStyle />
+      <BrowserRouter>
+        <AppLayout />
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
