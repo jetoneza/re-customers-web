11e9d5fdf2af164789fd5a947a58d9e943b4d2df
Jethro Ordaneza <jet.oneza@gmail.com>
2019-02-18 19:42:38 +0800

    Add style to NavBar

--- src/common/Nav.js
+++ src/common/Nav.js
@@ -1,37 +1,57 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-function getClassName({
-  dark,
-  fixedTop,
-  shadow
-}) {
-  // TODO: Improve this.
-  let className = 'navbar';
+// Constants
+import colors from 'constants/colors';
 
-  if (dark) {
-    className += ' navbar-dark bg-dark';
+const NavWrapper = styled.div.attrs({
+  className: 'nav-wrapper',
+})`
+  .navbar {
+    background-color: ${colors.primary};
+    padding: 20px;
+    box-sizing: border-box;
+    height: 60px;
+
+    .navbar-brand {
+      color: white;
+      font-size: 18px;
+      font-weight: 600;
+      line-height: 1;
+      text-decoration: none;
+    }
+
+    ${({ fixed }) => fixed ? (`
+      position: fixed;
+      top: 0;
+      right: 0;
+      left: 0;
+    `) : ''}
   }
+`;
 
-  if (shadow) {
-    className += ' shadow';
-  }
+NavWrapper.displayName = 'NavWrapper';
 
-  if (fixedTop) {
-    className += ' fixed-top';
-  }
-
-  return className;
-}
-
-export default function Nav({
+function Nav({
   children,
-  ...props
+  fixed,
 }) {
-  const className = getClassName(props);
-
   return (
-    <nav className={className}>
-      { children }
-    </nav>
+    <NavWrapper fixed={fixed}>
+      <nav className="navbar">
+        { children }
+      </nav>
+    </NavWrapper>
   );
 }
+
+Nav.propTypes = {
+  children: PropTypes.any,
+};
+
+Nav.defaultProps = {
+  children: null,
+};
+
+export default Nav;
