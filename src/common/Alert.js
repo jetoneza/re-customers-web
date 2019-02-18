a5a742754c3270151e746ea936e2422ac92a7870
Jethro Ordaneza <jet.oneza@gmail.com>
2019-02-18 18:52:11 +0800

    Implement alert color types

--- src/common/Alert.js
+++ src/common/Alert.js
@@ -1,19 +1,92 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import colors from 'constants/colors';
+
+const STYLES = {
+  primary: {
+    backgroundColor: colors.primaryBackground,
+    borderColor: colors.primaryBorder,
+    color: colors.primaryFont,
+  },
+  secondary: {
+    backgroundColor: colors.secondaryBackground,
+    borderColor: colors.secondaryBorder,
+    color: colors.secondaryFont,
+  },
+  success: {
+    backgroundColor: colors.successBackground,
+    borderColor: colors.successBorder,
+    color: colors.successFont,
+  },
+  danger: {
+    backgroundColor: colors.dangerBackground,
+    borderColor: colors.dangerBorder,
+    color: colors.dangerFont,
+  },
+  warning: {
+    backgroundColor: colors.warningBackground,
+    borderColor: colors.warningBorder,
+    color: colors.warningFont,
+  },
+  info: {
+    backgroundColor: colors.infoBackground,
+    borderColor: colors.infoBorder,
+    color: colors.infoFont,
+  },
+};
+
+const AlertWrapper = styled.div.attrs({
+  className: 'alert-wrapper',
+})`
+  padding: 12px 20px;
+  border-radius: 4px;
+  border: 1px solid ${ colors.gray };
+
+  ${({ type }) => {
+    const style = STYLES[type];
+
+    if (style) {
+      const {
+        backgroundColor,
+        borderColor,
+        color
+      } = style;
+
+      return (`
+        background-color: ${backgroundColor};
+        border-color: ${borderColor};
+        color: ${color};
+      `);
+    }
+  }}
+`;
+
+AlertWrapper.displayName = 'AlertWrapper';
 
 function Alert({
   children,
-  type
+  type,
 }) {
-  const className = `alert alert-${type || 'light'}`;
-
   return (
-    <div
-      className={className}
+    <AlertWrapper
+      type={type}
       role="alert"
     >
       {children}
-    </div>
+    </AlertWrapper>
   );
 }
+
+Alert.propTypes = {
+  children: PropTypes.any,
+  type: PropTypes.string,
+};
+
+Alert.defaultProps = {
+  children: null,
+  type: null,
+};
 
 export default Alert
