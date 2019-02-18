0e9cc9da89a0ec38e88f928b120cd4799766ad78
Jethro Ordaneza <jet.oneza@gmail.com>
2019-02-18 18:51:59 +0800

    Add box colors

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -8,6 +8,36 @@ const colors = {
   blueBerryDarker: '#3778C6',
   blueBerryDarkest: '#316BB0',
 
+  // Primary box colors
+  primaryFont: '#316BB0',
+  primaryBorder: '#76aff43d',
+  primaryBackground: '#5da1f34f',
+
+  // Secondary box colors
+  secondaryFont: '#262626',
+  secondaryBorder: '#2626260d',
+  secondaryBackground: '#2626261f',
+
+  // Success box colors
+  successFont: '#155724',
+  successBorder: '#c3e6cb',
+  successBackground: '#d4edda',
+
+  // Danger box colors
+  dangerFont: '#DC493A',
+  dangerBorder: '#dc493a14',
+  dangerBackground: '#dc493a38',
+
+  // Warning box colors
+  warningFont: '#856404',
+  warningBorder: '#ffeeba',
+  warningBackground: '#fff3cd',
+
+  // Info box colors
+  infoFont: '#2E80A7',
+  infoBorder: '#2e80a71a',
+  infoBackground: '#2e80a73d',
+
   // White-Black shades
   white: '#FFFFFF',
   platinum: '#E8E8E8',
