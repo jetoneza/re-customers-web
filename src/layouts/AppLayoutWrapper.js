b0b610bb46e9c3c50be962828ec5c5a3842bf7dd
Jethro Ordaneza <jet.oneza@gmail.com>
2019-02-25 00:02:45 +0800

    Adjust app layout style for mobile

--- src/layouts/AppLayoutWrapper.js
+++ src/layouts/AppLayoutWrapper.js
@@ -1,12 +1,19 @@
 import styled from 'styled-components';
 
+// Constants
+import { mediaMax } from 'constants/media';
+
 const AppLayoutWrapper = styled.div`
   .content {
     margin-top: 65px;
     margin-left: 200px;
     padding: 60px;
-    background: #f4f7fa;
     min-height: 100vh;
+
+    ${mediaMax.mobileL`
+      padding: 10px;
+      margin-left: 0;
+    `}
   }
 `;
 
