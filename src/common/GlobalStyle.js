891f598def845c5c679194d195f8dda3b37cf8bf
Jethro Ordaneza <jet.oneza@gmail.com>
2019-02-25 00:02:04 +0800

    Set body color

--- src/common/GlobalStyle.js
+++ src/common/GlobalStyle.js
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  body {
+    background: #f4f7fa;
+  }
+
   /********************
    * ReactModal Styles
    ********************/
